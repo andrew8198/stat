feb2262db9fbee2beb5cfae564d8b143086c5e0c
fwpsabermetrics <fwpsabermetrics@gmail.com>
2017-01-06 12:53:31 -0600

    Changed data_prep.R
    Added pseudo-code notes for Andrew.

--- data_prep.R
+++ data_prep.R
@@ -1,10 +1,17 @@
 library(openWAR)
-may <- getData(start="2015-05-01", end="2015-05-31")
-Andrew<-function(x){
-  (3*x) 
-}
-Andrew(3)
+library(dplyr)
+data <- getData(start="2015-05-01", end="2015-05-02")
+names(data)
+unique(data$event)
 
+###---PSEUDO CODE NOTES FOR ANDREW---###
+# We want to read in data for several seasons starting in 2005.
+# We want to re-code the event (Event)
+# We want to associate each Event with a value
+##    a. Bases
+##    b. Run value
+# We want to save this data as an RDS file.
+# saveRDS(data, "filename.RDS")
 
 
 
